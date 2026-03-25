e2e5b3f4a3ea519c636c15a2c8cb90418c8531d2
Rana <s-rana.abdelmawgoud@zewailcity.edu.eg>
2026-03-25 15:31:28 +0200

    add mlflow secret

--- train.py
+++ train.py
@@ -1,11 +1,14 @@
 import mlflow
-
-mlflow.set_tracking_uri("file:./mlruns")
+import os
+mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"]) 
 
 with mlflow.start_run() as run:
-    accuracy = 0.9  
+    accuracy = 0.9
 
     mlflow.log_metric("accuracy", accuracy)
+
+    with open("model_info.txt", "w") as f:
+        f.write(run.info.run_id)
 
     with open("accuracy.txt", "w") as f:
         f.write(str(accuracy))
